5c19b77608b981d54be1e04100a381ac7d6e55c3
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-21 17:23:17 -0500

    se actualizo las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Customers from '../views/Customers.vue'
 import Home from '../views/Home.vue'
+
+// Importar vistas principales
+import Customers from '../views/Customers.vue'
+import Categories from '../views/Categories.vue'
+import PayModes from '../views/PayModes.vue'
+import Products from '../views/Products.vue'
+
+// Importar componentes para crear/editar
 import NewCustomer from '../components/customers/NewCustomer.vue'
 import EditCustomer from '../components/customers/EditCustomer.vue'
+import NewCategory from '../components/categories/NewCategory.vue'
+import EditCategory from '../components/categories/EditCategory.vue'
+import NewPayMode from '../components/paymodes/NewPayMode.vue'
+import EditPayMode from '../components/paymodes/EditPayMode.vue'
+import NewProduct from '../components/products/NewProduct.vue'
+import EditProduct from '../components/products/EditProduct.vue'
 
 const routes = [
   {
@@ -10,6 +23,7 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  // Customers routes
   {
     path: '/customers',
     name: 'Customers',
@@ -24,6 +38,54 @@ const routes = [
     path: '/edit-customer/:id',
     name: 'EditCustomer',
     component: EditCustomer
+  },
+  // Categories routes
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: Categories
+  },
+  {
+    path: '/add-category',
+    name: 'NewCategory',
+    component: NewCategory
+  },
+  {
+    path: '/edit-category/:id',
+    name: 'EditCategory',
+    component: EditCategory
+  },
+  // Pay Modes routes
+  {
+    path: '/pay-modes',
+    name: 'PayModes',
+    component: PayModes
+  },
+  {
+    path: '/add-pay-mode',
+    name: 'NewPayMode',
+    component: NewPayMode
+  },
+  {
+    path: '/edit-pay-mode/:id',
+    name: 'EditPayMode',
+    component: EditPayMode
+  },
+  // Products routes
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/add-product',
+    name: 'NewProduct',
+    component: NewProduct
+  },
+  {
+    path: '/edit-product/:id',
+    name: 'EditProduct',
+    component: EditProduct
   }
 ]
 
