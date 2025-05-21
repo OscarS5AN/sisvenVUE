ac785d9d04a62a1c68aecd6ecfa19fa73d4743e9
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-21 16:23:27 -0500

    Se configuro vue en main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Importar Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+
+// Importar SweetAlert2
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+// Importar FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEdit, faTrash, faPlus, faSave, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faEdit, faTrash, faPlus, faSave, faTimes)
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueSweetalert2)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
